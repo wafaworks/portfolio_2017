c2eafa7903e0ed853b19fa8d18b502a2ada83702
wafaworks <wafa.adj@gmail.com>
2017-04-12 19:57:28 +0200

    New bloc for competences and added animations on some blocks

--- app/Resources/front_end/dev/js/main.js
+++ app/Resources/front_end/dev/js/main.js
@@ -1,68 +1,66 @@
-/*
- * Replace all SVG images with inline SVG
- */
-jQuery('img.svg').each(function(){
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
+$(document).ready(function() {
 
-    jQuery.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
+    /*
+     * Replace all SVG images with inline SVG
+     */
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
 
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
 
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
 
-        // Replace image with new SVG
-        $img.replaceWith($svg);
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
 
-    }, 'xml');
+            // Replace image with new SVG
+            $img.replaceWith($svg);
 
-});
+        }, 'xml');
 
-/*
- * Smooth scrolling
- */
-
-function scrollNav() {
-    $('.nav a').click(function(){
-        //Toggle Class
-        $(".active").removeClass("active");
-        $(this).closest('li').addClass("active");
-        var theClass = $(this).attr("class");
-        $('.'+theClass).parent('li').addClass('active');
-        //Animate
-        $('html, body').stop().animate({
-            scrollTop: $( $(this).attr('href') ).offset().top - 160
-        }, 400);
-        return false;
     });
-    $('.scrollTop a').scrollTop();
-}
-scrollNav();
 
+    /*
+     * Word auto changing every 2 sec
+     */
 
-/*
- * Word auto changing every 2 sec
- */
+    $(function () {
+        words = ["lettering", "music", "movies", "animals", "meditation"];
+        count = 0;
+        setInterval(function () {
+            count++;
+            $(".word-auto-change").fadeOut(400, function () {
+                $(this).text(words[count % words.length]).fadeIn(400);
+            })
+        }, 2000);
+    });
 
-$(function(){
-    words = ["lettering", "music", "movies", "animals", "meditation"];
-    count = 0;
-    setInterval(function () {
-        count++;
-        $(".word-auto-change").fadeOut(400, function () {
-            $(this).text(words[count % words.length]).fadeIn(400);
-        })
-    }, 2000);
+    /*
+     * Smooth scrolling
+     */
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
 });
